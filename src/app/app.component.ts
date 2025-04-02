e541b30abc28fde6968a3b7895f1c344ac369fac
Tim Batty <tbatty7@gmail.com>
2025-04-02 16:38:30 -0400

    List the items being added to the parent from the child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ChildComponent} from "./child/child.component";
+import {of} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,6 @@ export class AppComponent {
   title = 'angular-demo';
   items: string[] = [];
   addItem(item: string) {this.items.push(item);}
+
+  protected readonly of = of;
 }
