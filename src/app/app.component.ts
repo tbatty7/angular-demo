17357a856f346a6e4a9b51ec2eb15c2996c45597
Tim Batty <tbatty7@gmail.com>
2025-04-02 17:18:24 -0400

    Implemented two-way binding with ngModel and input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ChildComponent} from "./child/child.component";
 import {of} from "rxjs";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChildComponent],
+  imports: [RouterOutlet, ChildComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
